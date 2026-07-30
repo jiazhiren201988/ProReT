8320ca63c35ee42afd6bb01b3af49a7b7756385c
jiazhiren201988 <jiazhiren201988@gmail.com>
2026-07-30 17:08:22 +0800

    Delete ProReT directory

--- ProReT/R/globals.R
+++
@@ -1,5 +0,0 @@
-utils::globalVariables(c(
-  ".", ".N", ".SD", ":=", "cell_iname", "drug", "is_hiq",
-  "n_cell_lines", "pert_id", "pert_idose", "pert_iname", "pert_itime",
-  "reversal_score", "score", "sig_id"
-))
